34c9f9b677230dfb26e06badbcbe51ce8a34fcfe
omarelsabaawy <omarelsabaawy77@gmail.com>
2023-12-09 17:19:46 +0200

    Created the frontend structure for the form allowing bikers to input timestamps for pickup and delivery for each order. This enhancement enables bikers to efficiently manage order scheduling.

---
+++ client/src/Components/BikerPages/BikerDashboard.jsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Card, CardActionArea, CardActions, CardContent, CircularProgress, Grid, Paper, Typography } from '@mui/material';
+import { useUser } from '../../Context/UserContext';
+import { listParcels } from '../../Services/Captains/ListParcels';
+import ParcelDetails from '../ParcelDetails';
+import SelectParcel from '../SelectParcel';
+
+
+function BikerDashboard() {
+    const [orders, setOrders] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [viewOrder, setViewOrder] = useState(null);
+    const [selectOrder, setSelectOrder] = useState(null);
+
+
+    const handleOpen = (order) => {
+        setViewOrder(order);
+    };
+
+    const handleClose = () => {
+        setViewOrder(null);
+    };
+
+    const handleOpenSelectOrder = (order) => {
+        setSelectOrder(order);
+    };
+
+    const handleCloseSelectOrder = () => {
+        setSelectOrder(null);
+    };
+
+    const { user } = useUser();
+
+    useEffect(() => {
+        const fetchOrders = async () => {
+            try {
+                setLoading(true);
+                const result = await listParcels(user.token);
+                setOrders(result);
+                setLoading(false);
+            } catch (error) {
+                console.error('Failed to fetch parcel data:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchOrders();
+    }, [user.token]);
+
+    return (
+        <Grid container sx={{ marginTop: 3 }}>
+            <Grid item md={12} lg={11.5}>
+                <Paper
+                    sx={{
+                        p: 2,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: 460,
+                        overflowY: 'auto',
+                    }}
+                >
+                    <Typography component="h2" variant="h6" color="primary" gutterBottom>
+                        Recent Orders
+                    </Typography>
+                    {loading ? (
+                        <Grid container spacing={2}>
+                            <Grid item xs={6} md={4}>
+                                <CircularProgress />
+                            </Grid>
+                        </Grid>
+                    ) : (
+                        <Grid container spacing={2}>
+                            {orders.map((order) => (
+                                <Grid item xs={6} md={4} key={order.parcelId}>
+                                    <Card sx={{ maxWidth: 345 }}>
+                                        <CardActionArea onClick={() => handleOpen(order)}>
+                                            <CardContent>
+                                                <Typography gutterBottom sx={{ fontSize: '18px' }} component="div">
+                                                    Order id <span style={{ fontSize: '16px', textDecoration: 'underline' }}>{order.parcelId}</span>
+                                                </Typography>
+                                                <Typography sx={{ fontSize: '16' }} color="text.secondary">
+                                                    From: {order.pickUpAddress}
+                                                </Typography>
+                                                <Typography sx={{ fontSize: '16' }} color="text.secondary">
+                                                    to: {order.dropOffAddress}
+                                                </Typography>
+                                            </CardContent>
+                                        </CardActionArea>
+                                        <CardActions sx={{ justifyContent: 'center' }}>
+                                            <Button color="success" variant="outlined" size="medium" onClick={() => handleOpenSelectOrder(order)}>
+                                                Select the order
+                                            </Button>
+                                        </CardActions>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    )}
+                </Paper>
+            </Grid>
+            {viewOrder && (
+                <ParcelDetails open={true} handleClose={handleClose} order={viewOrder} />
+            )}
+            {selectOrder && (
+                <SelectParcel open={true} handleClose={handleCloseSelectOrder} order={selectOrder} />
+            )}
+
+        </Grid>
+    );
+}
+
+export default BikerDashboard;
---
+++ client/src/Components/SelectParcel.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { useUser } from '../Context/UserContext';
+import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Typography from '@mui/material/Typography';
+import { Button, Grid, TextField } from '@mui/material';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    height: 320,
+    backgroundColor: "#1976D2",
+    borderColor: "#1976D2",
+    color: 'white',
+    borderRadius: "1rem",
+    boxShadow: 24,
+    p: 4,
+};
+
+function SelectParcel({ open, handleClose, order }) {
+    const [pickUpDate, setPickUpDate] = useState("");
+    const [dropOffDate, setDropOffDate] = useState("");
+
+    const { user } = useUser();
+
+    const navigate = useNavigate();
+
+    const handleSelectOrder = async (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+        >
+            <Fade in={open}>
+                <Box sx={style}>
+                    <Typography id="transition-modal-title" sx={{ textAlign: 'center' }} variant="h5">
+                        Select This Order
+                    </Typography>
+                    <Typography id="transition-modal-title" sx={{ textAlign: 'center', marginTop: '20px' }} variant="h6">
+                        Order Id {order.parcelId}
+                    </Typography>
+                    <Grid container sx={{ marginTop: '30px' }}>
+                        <Grid sx={{ fontSize: '18px', marginTop: '5px' }} item xs={6} md={6}>
+                            Pick up date:
+                        </Grid>
+                        <Grid sx={{ fontSize: '18px' }} item xs={6} md={6}>
+                            <TextField type='date' size="small" value={pickUpDate} onChange={(e) => setPickUpDate(e.target.value)} />
+                        </Grid>
+                    </Grid>
+                    <Grid container sx={{ marginTop: '30px' }}>
+                        <Grid sx={{ fontSize: '18px', marginTop: '5px' }} item xs={6} md={6}>
+                            Delivery date:
+                        </Grid>
+                        <Grid sx={{ fontSize: '18px' }} item xs={6} md={6}>
+                            <TextField type='date' size="small" value={dropOffDate} onChange={(e) => setDropOffDate(e.target.value)} />
+                        </Grid>
+                    </Grid>
+                    <Grid container sx={{ marginTop: '30px', justifyContent: 'center' }}>
+                        <Button color="info" variant="contained" onClick={handleSelectOrder}>Select Order</Button>
+                    </Grid>
+                </Box>
+            </Fade>
+        </Modal>
+    );
+}
+
+export default SelectParcel;
